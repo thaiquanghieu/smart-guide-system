c115b5b9d4d2ad09357047baf601ffca0bdd60f5
QHieu <thaiquanghieu0126@gmail.com>
2026-04-21 19:53:31 +0700

    feat: switch local api to api public by ngrok

--- SmartGuideApp/Config/AppEndpoints.cs
+++ SmartGuideApp/Config/AppEndpoints.cs
@@ -2,7 +2,9 @@ namespace SmartGuideApp.Config;
 
 public static class AppEndpoints
 {
-    public const string ApiBaseUrl = "http://192.168.22.4:5022";
+    // Dán URL public của ngrok hoặc host API vào đây, ví dụ:
+    // https://abc123.ngrok-free.app
+    public const string ApiBaseUrl = "https://comrade-matter-shell.ngrok-free.dev";
 
     public static string BuildApiUrl(string path)
     {
