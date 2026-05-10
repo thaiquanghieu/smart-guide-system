28d3b60e2d2856034dc8a8d117c3205c0e4ba65e
QHieu <thaiquanghieu0126@gmail.com>
2026-05-10 14:17:12 +0700

    Add Cloudinary upload smoke test endpoint

--- services/api/Controllers/DebugController.cs
+++ services/api/Controllers/DebugController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using SmartGuideAPI.Services;
+using Microsoft.AspNetCore.Http;
 
 namespace SmartGuideAPI.Controllers;
 
@@ -28,5 +29,38 @@ public class DebugController : ControllerBase
             hasUploadPreset = debugInfo.HasUploadPreset,
             mode = debugInfo.Strategy
         });
+    }
+
+    [HttpGet("cloudinary-upload-test")]
+    public async Task<IActionResult> TestCloudinaryUpload()
+    {
+        try
+        {
+            var pngBytes = Convert.FromBase64String(
+                "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+lmVsAAAAASUVORK5CYII="
+            );
+
+            await using var stream = new MemoryStream(pngBytes);
+            var formFile = new FormFile(stream, 0, pngBytes.Length, "file", "debug.png")
+            {
+                Headers = new HeaderDictionary(),
+                ContentType = "image/png"
+            };
+
+            var url = await _mediaStorageService.UploadImageAsync(formFile, "debug", "cloudinary-test");
+            return Ok(new
+            {
+                success = true,
+                url
+            });
+        }
+        catch (Exception exception)
+        {
+            return StatusCode(500, new
+            {
+                success = false,
+                message = exception.Message
+            });
+        }
     }
 }
